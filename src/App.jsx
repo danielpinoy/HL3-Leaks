fb418894a1b18c2c231ecb2907e35eeb49a27808
danielpinoy <almirante.danieljohn@gmail.com>
2025-03-30 00:48:48 +0200

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-
 // Import pages
 import HomePage from "./pages/HomePage";
 import CategoryPage from "./pages/CategoryPage";
